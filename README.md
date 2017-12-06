42b95601a9db88a28ee0e689cf2772c0be8a8b59
Jakub Bouček <pan@jakubboucek.cz>
2017-12-06 06:17:01 +0100

    Init; v0.1

---
+++ README.md
@@ -0,0 +1,11 @@
+# [Ulož.to](https://ulozto.cz/) improve player
+Google Chrome extension for improve video player on Uloz.to
+
+## Install to Google Chrome
+Install from Chrome Web Store: [**Uloz.to improve player**](https://chrome.google.com/webstore/detail/ulozto-improve-player/tobecompletedlater)
+
+## Features
+- Allow un/pause video by Space key
+
+## Planned features
+- Seek on video by Arrow keys (like YouTube) 
---
+++ ulozto-player.js
@@ -0,0 +1,110 @@
+'use strict';
+
+(function (_window, _body) {
+
+    window.addEventListener('keydown', keyDownHandler, true);
+    window.addEventListener('keypress', keyPressHandler, true);
+
+    /**
+     * Receive all key event and filter only non-form (no to try catching passwords)
+     * @param {KeyboardEvent} event
+     * @throws {NoVideoException}
+     */
+    function keyDownHandler(event) {
+        if (!isAcceptableEvent(event)) {
+            return;
+        }
+
+        try {
+            switch (event.code) {
+                case 'Space':
+                    spaceHandler(event);
+                    break;
+            }
+        }
+        catch (error) {
+            if (!(error instanceof NoVideoException)) {
+                throw error;
+            }
+        }
+    }
+
+    /**
+     * Receive all key event and prevent default behavior on some of there
+     * @param {KeyboardEvent} event
+     */
+    function keyPressHandler(event) {
+        if (!isAcceptableEvent(event)) {
+            return;
+        }
+
+        switch (event.code) {
+            case 'Space':
+                event.preventDefault();
+                break;
+        }
+    }
+
+    /**
+     * Un/pause video
+     * @param {KeyboardEvent} event
+     * @throws {NoVideoException}
+     */
+    function spaceHandler(event) {
+        const video = getVideoElement();
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+
+    /**
+     * Get video element or throw exception if not exists
+     * @returns {HTMLVideoElement}
+     * @throws {NoVideoException}
+     */
+    function getVideoElement() {
+        const videoElement = document.querySelector('div.stream-player.is-playing  > video');
+        if (videoElement === null) {
+            throw new NoVideoException();
+        }
+        return videoElement;
+    }
+
+    /**
+     * Return true when event acceptable for processing
+     * @param {KeyboardEvent} event
+     * @returns {boolean}
+     */
+    function isAcceptableEvent(event) {
+        return isVideoEvent(event) && isSimpleEvent(event);
+    }
+
+    /**
+     * Return true when event not target to form, button or link
+     * @param {KeyboardEvent} event
+     * @returns {boolean}
+     */
+    function isVideoEvent(event) {
+        return (event.target === _body);
+    }
+
+    /**
+     * Return true when event is not key-combined shortcut
+     * @param {KeyboardEvent} event
+     * @returns {boolean}
+     */
+    function isSimpleEvent(event) {
+        return !(event.altKey || event.ctrlKey || event.metaKey || event.shiftKey);
+    }
+
+    /**
+     * Class of Exception - thrown when no video element currently presented on page
+     * @constructor
+     */
+    function NoVideoException() {
+        this.name = 'NoVideoException';
+    }
+
+})(window, document.body);
